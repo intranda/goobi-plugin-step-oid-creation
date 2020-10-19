a95f75d2e291f8cff244a10d2f5daa4aac045fcd
Robert Sehr <robert.sehr@intranda.com>
2020-10-19 15:39:41 +0200

    initial commit

---
+++ goobi-plugin-step-oid-creation/src/main/java/de/intranda/goobi/plugins/OidStepPlugin.java
@@ -0,0 +1,284 @@
+package de.intranda.goobi.plugins;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.goobi.beans.Process;
+import org.goobi.beans.Step;
+import org.goobi.production.enums.PluginGuiType;
+import org.goobi.production.enums.PluginReturnValue;
+import org.goobi.production.enums.PluginType;
+import org.goobi.production.enums.StepReturnValue;
+import org.goobi.production.plugin.interfaces.IStepPluginVersion2;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import de.sub.goobi.helper.HttpClientHelper;
+import de.sub.goobi.helper.StorageProvider;
+import de.sub.goobi.helper.exceptions.DAOException;
+import de.sub.goobi.helper.exceptions.SwapException;
+import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+import ugh.dl.DocStruct;
+import ugh.dl.Fileformat;
+import ugh.dl.Metadata;
+import ugh.dl.MetadataType;
+import ugh.dl.Prefs;
+import ugh.exceptions.DocStructHasNoTypeException;
+import ugh.exceptions.MetadataTypeNotAllowedException;
+import ugh.exceptions.PreferencesException;
+import ugh.exceptions.ReadException;
+import ugh.exceptions.WriteException;
+
+@PluginImplementation
+@Log4j2
+public class OidStepPlugin implements IStepPluginVersion2 {
+
+    @Getter
+    private String title = "intranda_step_oid";
+
+    @Getter
+    private Step step;
+    Process process;
+    Prefs prefs;
+    @Getter
+    private PluginGuiType pluginGuiType = PluginGuiType.NONE;
+
+    @Getter
+    private String pagePath = null;
+
+    @Getter
+    private PluginType type = PluginType.Step;
+
+    @Getter
+    private int interfaceVersion = 0;
+
+    @Override
+    public String cancel() {
+        return null;
+    }
+
+    @Override
+    public boolean execute() {
+        if (run() != PluginReturnValue.FINISH) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public String finish() {
+        return null;
+    }
+
+    @Override
+    public void initialize(Step step, String returnPath) {
+        this.step = step;
+        process = step.getProzess();
+        prefs = process.getRegelsatz().getPreferences();
+
+    }
+
+    @Override
+    public HashMap<String, StepReturnValue> validate() {
+        return null;
+    }
+
+    @Override
+    public PluginReturnValue run() {
+
+        String url = "http://localhost:8080/goobi/uii/";
+        String username = "user";
+        String password = "password";
+        // open metadata file
+        MetadataType identifierType = prefs.getMetadataTypeByName("CatalogIDDigital");
+        MetadataType contentIdsType = prefs.getMetadataTypeByName("_urn");
+        int numberOfOids = 0;
+
+        try {
+            Fileformat fileformat = process.readMetadataFile();
+            // get main object
+            DocStruct logical = fileformat.getDigitalDocument().getLogicalDocStruct();
+            // check if main element contains a OID
+
+            List<? extends Metadata> mdl = logical.getAllMetadataByType(identifierType);
+            Metadata identifier = null;
+            boolean skipMainElement = false;
+            if (!mdl.isEmpty()) {
+                identifier = mdl.get(0);
+                if (identifier.getValue().matches("\\d+") && identifier.getValue().length() <= 9) {
+                    skipMainElement = true;
+                }
+            }
+
+            if (!skipMainElement) {
+                numberOfOids++;
+            }
+
+            // get page objects
+            DocStruct physical = fileformat.getDigitalDocument().getPhysicalDocStruct();
+            List<DocStruct> pageList = physical.getAllChildren();
+
+            // get OIDs only for objects without ids
+            for (DocStruct page : pageList) {
+                List<? extends Metadata> urns = page.getAllMetadataByType(contentIdsType);
+                if (urns.isEmpty()) {
+                    numberOfOids++;
+                }
+            }
+            if (numberOfOids == 0) {
+                // all fields contain OIDs, finish
+                return PluginReturnValue.FINISH;
+            }
+            // request number of pages + 1
+            if (numberOfOids != pageList.size() + 1) {
+                // TODO WARNING; we update an existing object
+                // write something to processlog
+            }
+            numberOfOids = pageList.size() + 1;
+
+            // get new identifiers from list
+            String response = getStringFromUrl(url + numberOfOids + ".json", username, password, "", "");
+            JsonElement jsonTree = JsonParser.parseString(response);
+
+            JsonObject jsonObject = jsonTree.getAsJsonObject();
+            JsonElement oids = jsonObject.get("oids");
+            JsonArray values = oids.getAsJsonArray();
+
+            int counter = 0;
+
+            if (!skipMainElement) {
+                Long oid = values.get(counter).getAsLong();
+                if (identifier == null) {
+                    try {
+                        identifier = new Metadata(identifierType);
+                        logical.addMetadata(identifier);
+                    } catch (MetadataTypeNotAllowedException | DocStructHasNoTypeException e) {
+                        log.error(e);
+                    }
+                }
+                identifier.setValue(String.valueOf(oid));
+                counter++;
+            }
+
+            // get all folders to check and rename images
+            Map<Path, List<Path>> allFolderAndAllFiles = process.getAllFolderAndFiles();
+            // check size of folders, remove them if they don't match the expected number of files
+            for (Path p : allFolderAndAllFiles.keySet()) {
+                List<Path> files = allFolderAndAllFiles.get(p);
+                if (pageList.size() != files.size()) {
+                    files = Collections.emptyList();
+                }
+            }
+
+            for (DocStruct page : pageList) {
+                List<? extends Metadata> urns = page.getAllMetadataByType(contentIdsType);
+                if (urns.isEmpty()) {
+                    try {
+                        Metadata urn = new Metadata(contentIdsType);
+                        Long oid = values.get(counter).getAsLong();
+                        urn.setValue(String.valueOf(oid));
+                        page.addMetadata(urn);
+                        // rename images to OID.extension
+                        String oldFilename = page.getImageName();
+                        Path f = Paths.get(oldFilename).getFileName();
+                        oldFilename = f.toString();
+                        int dot = oldFilename.lastIndexOf('.');
+                        String basename = (dot == -1) ? oldFilename : oldFilename.substring(0, dot);
+                        String extension = (dot == -1) ? "" : oldFilename.substring(dot + 1);
+
+                        for (Path currentFolder : allFolderAndAllFiles.keySet()) {
+                            // check files in current folder
+                            List<Path> files = allFolderAndAllFiles.get(currentFolder);
+                            for (Path file : files) {
+                                String filenameToCheck = file.getFileName().toString();
+                                String filenamePrefixToCheck = filenameToCheck.substring(0, filenameToCheck.lastIndexOf("."));
+                                String fileExtension = filenameToCheck.substring(filenameToCheck.lastIndexOf(".") + 1);
+                                // find the current file the folder
+                                if (filenamePrefixToCheck.equals(basename)) {
+                                    // found file to rename, create new file name
+                                    Path tmpFileName = Paths.get(currentFolder.toString(), urn.getValue() + "." + fileExtension);
+                                    try {
+                                        StorageProvider.getInstance().move(file, tmpFileName);
+                                    } catch (IOException e) {
+                                        log.error(e);
+                                    }
+                                }
+                            }
+                        }
+
+                        // write new image names to page objects
+                        page.setImageName(urn.getValue() + "." + extension);
+
+                        counter++;
+                    } catch (MetadataTypeNotAllowedException | DocStructHasNoTypeException e) {
+                        log.error(e);
+                    }
+                }
+            }
+
+            // save metadata file
+            process.writeMetadataFile(fileformat);
+
+        } catch (ReadException | PreferencesException | WriteException | IOException | InterruptedException | SwapException | DAOException e) {
+            log.error(e);
+        }
+        return PluginReturnValue.FINISH;
+    }
+
+    static String getStringFromUrl(String url, String username, String password, String headerParam, String headerParamValue) {
+        String response = "";
+        CloseableHttpClient client = null;
+        HttpGet method = new HttpGet(url);
+
+        if (username != null && password != null) {
+            CredentialsProvider credsProvider = new BasicCredentialsProvider();
+            credsProvider.setCredentials(new AuthScope(null, -1), new UsernamePasswordCredentials(username, password));
+            client = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+
+        } else {
+            client = HttpClientBuilder.create().build();
+        }
+
+        if (headerParam != null) {
+            // add header parameter
+            method.setHeader(headerParam, headerParamValue);
+        }
+
+        try {
+            response = client.execute(method, HttpClientHelper.stringResponseHandler);
+        } catch (IOException e) {
+            log.error("Cannot execute URL " + url, e);
+        } finally {
+            method.releaseConnection();
+
+            if (client != null) {
+                try {
+                    client.close();
+                } catch (IOException e) {
+                    log.error(e);
+                }
+            }
+        }
+        return response;
+    }
+
+}
---
+++ goobi-plugin-step-oid-creation/src/test/java/de/intranda/goobi/plugins/OidPluginTest.java
@@ -0,0 +1,262 @@
+package de.intranda.goobi.plugins;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.easymock.EasyMock;
+import org.goobi.beans.Process;
+import org.goobi.beans.Project;
+import org.goobi.beans.Ruleset;
+import org.goobi.beans.Step;
+import org.goobi.beans.User;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import de.sub.goobi.config.ConfigurationHelper;
+import de.sub.goobi.helper.VariableReplacer;
+import de.sub.goobi.helper.enums.StepStatus;
+import de.sub.goobi.metadaten.MetadatenHelper;
+import de.sub.goobi.persistence.managers.MetadataManager;
+import de.sub.goobi.persistence.managers.ProcessManager;
+import ugh.dl.DocStruct;
+import ugh.dl.Fileformat;
+import ugh.dl.Prefs;
+import ugh.fileformats.mets.MetsMods;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ MetadatenHelper.class, VariableReplacer.class, ConfigurationHelper.class, ProcessManager.class, MetadataManager.class,
+    OidStepPlugin.class })
+@PowerMockIgnore({ "javax.management.*", "javax.net.ssl.*" })
+
+public class OidPluginTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+    private File processDirectory;
+    private File metadataDirectory;
+    private Process process;
+    private Prefs prefs;
+    private String resourcesFolder;
+
+    @Before
+    public void setUp() throws Exception {
+        resourcesFolder = "src/test/resources/"; // for junit tests in eclipse
+
+        if (!Files.exists(Paths.get(resourcesFolder))) {
+            resourcesFolder = "target/test-classes/"; // to run mvn test from cli or in jenkins
+        }
+
+        metadataDirectory = folder.newFolder("metadata");
+
+        processDirectory = new File(metadataDirectory + File.separator + "1");
+        processDirectory.mkdirs();
+        String metadataDirectoryName = metadataDirectory.getAbsolutePath() + File.separator;
+
+        // copy meta.xml
+        Path metaSource = Paths.get(resourcesFolder + "meta.xml");
+        Path metaTarget = Paths.get(processDirectory.getAbsolutePath(), "meta.xml");
+
+        Files.copy(metaSource, metaTarget);
+
+        PowerMock.mockStatic(ConfigurationHelper.class);
+        ConfigurationHelper configurationHelper = EasyMock.createMock(ConfigurationHelper.class);
+        EasyMock.expect(ConfigurationHelper.getInstance()).andReturn(configurationHelper).anyTimes();
+        EasyMock.expect(configurationHelper.getMetsEditorLockingTime()).andReturn(1800000l).anyTimes();
+        EasyMock.expect(configurationHelper.isAllowWhitespacesInFolder()).andReturn(false).anyTimes();
+        EasyMock.expect(configurationHelper.useS3()).andReturn(false).anyTimes();
+        EasyMock.expect(configurationHelper.isUseProxy()).andReturn(false).anyTimes();
+        EasyMock.expect(configurationHelper.getGoobiContentServerTimeOut()).andReturn(60000).anyTimes();
+        EasyMock.expect(configurationHelper.getMetadataFolder()).andReturn(metadataDirectoryName).anyTimes();
+        EasyMock.expect(configurationHelper.getProcessImagesMainDirectoryName()).andReturn("00469418X_media").anyTimes();
+        EasyMock.expect(configurationHelper.isUseMasterDirectory()).andReturn(true).anyTimes();
+
+        EasyMock.expect(configurationHelper.getNumberOfMetaBackups()).andReturn(0).anyTimes();
+        EasyMock.replay(configurationHelper);
+
+        PowerMock.mockStatic(VariableReplacer.class);
+        EasyMock.expect(VariableReplacer.simpleReplace(EasyMock.anyString(), EasyMock.anyObject())).andReturn("00469418X_media").anyTimes();
+        PowerMock.replay(VariableReplacer.class);
+        prefs = new Prefs();
+        prefs.loadPrefs(resourcesFolder + "vd18.xml");
+        Fileformat ff = new MetsMods(prefs);
+        ff.read(metaTarget.toString());
+
+        PowerMock.mockStatic(MetadatenHelper.class);
+        EasyMock.expect(MetadatenHelper.getMetaFileType(EasyMock.anyString())).andReturn("mets").anyTimes();
+        EasyMock.expect(MetadatenHelper.getFileformatByName(EasyMock.anyString(), EasyMock.anyObject())).andReturn(ff).anyTimes();
+        EasyMock.expect(MetadatenHelper.getMetadataOfFileformat(EasyMock.anyObject(), EasyMock.anyBoolean()))
+        .andReturn(Collections.emptyMap())
+        .anyTimes();
+        PowerMock.replay(MetadatenHelper.class);
+
+        PowerMock.mockStatic(MetadataManager.class);
+        MetadataManager.updateMetadata(1, Collections.emptyMap());
+        MetadataManager.updateJSONMetadata(1, Collections.emptyMap());
+        PowerMock.replay(MetadataManager.class);
+        PowerMock.replay(ConfigurationHelper.class);
+
+        PowerMock.mockStatic(OidStepPlugin.class);
+
+        EasyMock.expect(OidStepPlugin.getStringFromUrl(EasyMock.anyString(), EasyMock.anyString(), EasyMock.anyString(), EasyMock.anyString(),
+                EasyMock.anyString())).andReturn("{\"oids\":[300006252,300006253,300006254,300006255,300006256,300006257]}").anyTimes();
+        process = getProcess();
+
+        Ruleset ruleset = PowerMock.createMock(Ruleset.class);
+        ruleset.setTitel("vd18");
+        ruleset.setDatei("vd18.xml");
+        process.setRegelsatz(ruleset);
+        EasyMock.expect(ruleset.getPreferences()).andReturn(prefs).anyTimes();
+        PowerMock.replay(ruleset);
+
+        PowerMock.replay(OidStepPlugin.class);
+
+    }
+
+    @Test
+    public void testConstructor() throws IOException {
+        OidStepPlugin plugin = new OidStepPlugin();
+        assertNotNull(plugin);
+    }
+
+    @Test
+    public void testInit() {
+        OidStepPlugin plugin = new OidStepPlugin();
+        assertNotNull(plugin);
+        Step step = process.getSchritte().get(0);
+        plugin.initialize(step, "");
+
+        assertEquals("test step", plugin.getStep().getTitel());
+        assertEquals("00469418X", plugin.process.getTitel());
+        assertNotNull(plugin.prefs);
+
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+        OidStepPlugin plugin = new OidStepPlugin();
+        assertNotNull(plugin);
+        Step step = process.getSchritte().get(0);
+
+        plugin.initialize(step, "");
+
+        // check old file names
+
+        String[] filesInMasterFolder = new File(processDirectory.getAbsolutePath() + "/images/00469418X_master").list();
+        Arrays.sort(filesInMasterFolder);
+        assertEquals("00000001.tif", filesInMasterFolder[0]);
+
+        String[] filesInMediaFolder = new File(processDirectory.getAbsolutePath() + "/images/00469418X_media").list();
+        Arrays.sort(filesInMediaFolder);
+        assertEquals("00000001.jpg", filesInMediaFolder[0]);
+
+        assertTrue(plugin.execute());
+
+        // open created file
+        Fileformat ff = new MetsMods(prefs);
+        ff.read(processDirectory.getAbsolutePath() +"/meta.xml");
+        // check oids in mets file
+
+        DocStruct log = ff.getDigitalDocument().getLogicalDocStruct();
+        assertEquals("300006252", log.getAllMetadataByType(prefs.getMetadataTypeByName("CatalogIDDigital")).get(0).getValue());
+
+        // check Page DocStructs
+        DocStruct page = ff.getDigitalDocument().getPhysicalDocStruct().getAllChildren().get(0);
+        assertEquals("300006253", page.getAllMetadataByType(prefs.getMetadataTypeByName("_urn")).get(0).getValue());
+        assertEquals("300006253.jpg", page.getImageName());
+
+        // check file names
+        filesInMasterFolder = new File(processDirectory.getAbsolutePath() + "/images/00469418X_master").list();
+        Arrays.sort(filesInMasterFolder);
+        assertEquals("300006253.tif", filesInMasterFolder[0]);
+
+        filesInMediaFolder = new File(processDirectory.getAbsolutePath() + "/images/00469418X_media").list();
+        Arrays.sort(filesInMediaFolder);
+        assertEquals("300006253.jpg", filesInMediaFolder[0]);
+
+    }
+
+    public Process getProcess() {
+        Project project = new Project();
+        project.setTitel("SampleProject");
+
+        Process process = new Process();
+        process.setTitel("00469418X");
+        process.setProjekt(project);
+        process.setId(1);
+        List<Step> steps = new ArrayList<>();
+        Step s1 = new Step();
+        s1.setReihenfolge(1);
+        s1.setProzess(process);
+        s1.setTitel("test step");
+        s1.setBearbeitungsstatusEnum(StepStatus.OPEN);
+        User user = new User();
+        user.setVorname("Firstname");
+        user.setNachname("Lastname");
+        s1.setBearbeitungsbenutzer(user);
+        steps.add(s1);
+
+        process.setSchritte(steps);
+
+        try {
+            createProcessDirectory(processDirectory);
+        } catch (IOException e) {
+        }
+
+        return process;
+    }
+
+    private void createProcessDirectory(File processDirectory) throws IOException {
+
+        // image folder
+        File imageDirectory = new File(processDirectory.getAbsolutePath(), "images");
+        imageDirectory.mkdir();
+        // master folder
+        File masterDirectory = new File(imageDirectory.getAbsolutePath(), "00469418X_master");
+        masterDirectory.mkdir();
+        {
+            File image1 = new File(masterDirectory.getAbsoluteFile(), "00000001.tif");
+            File image2 = new File(masterDirectory.getAbsoluteFile(), "00000002.tif");
+            File image3 = new File(masterDirectory.getAbsoluteFile(), "00000003.tif");
+            File image4 = new File(masterDirectory.getAbsoluteFile(), "00000004.tif");
+            File image5 = new File(masterDirectory.getAbsoluteFile(), "00000005.tif");
+            image1.createNewFile();
+            image2.createNewFile();
+            image3.createNewFile();
+            image4.createNewFile();
+            image5.createNewFile();
+        }
+        // media folder
+        File mediaDirectory = new File(imageDirectory.getAbsolutePath(), "00469418X_media");
+        mediaDirectory.mkdir();
+        {
+            File image1 = new File(mediaDirectory.getAbsoluteFile(), "00000001.jpg");
+            File image2 = new File(mediaDirectory.getAbsoluteFile(), "00000002.jpg");
+            File image3 = new File(mediaDirectory.getAbsoluteFile(), "00000003.jpg");
+            File image4 = new File(mediaDirectory.getAbsoluteFile(), "00000004.jpg");
+            File image5 = new File(mediaDirectory.getAbsoluteFile(), "00000005.jpg");
+            image1.createNewFile();
+            image2.createNewFile();
+            image3.createNewFile();
+            image4.createNewFile();
+            image5.createNewFile();
+        }
+    }
+}
